c329c5625bd9c9ac1410d8e8f5004f161b370c21
cthompson71 <40264316+Cameron-Thompson@users.noreply.github.com>
2024-10-20 01:06:45 +0100

    add audio

--- Create with Code Part 2 Mission 1 - Sound and Effects/Unit3/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 2 Mission 1 - Sound and Effects/Unit3/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,21 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody playerRb;
     private Animator playerAnim;
+    private AudioSource playerAudio;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -26,6 +33,8 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound,1.0f);
         }
     }
 
@@ -37,13 +46,16 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
-
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound,1.0f);
             Debug.Log("Game Over");
         }
     }
