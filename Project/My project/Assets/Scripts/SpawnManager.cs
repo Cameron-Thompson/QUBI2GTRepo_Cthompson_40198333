3940cf17c524ce80b52b1932dd42edc5813be1d5
cthompson71 <40264316+Cameron-Thompson@users.noreply.github.com>
2024-10-27 11:02:54 +0000

    Fix spawn rate

--- Project/My project/Assets/Scripts/SpawnManager.cs
+++ Project/My project/Assets/Scripts/SpawnManager.cs
@@ -4,31 +4,40 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    private float spawnRangeX = 57;
+    private float spawnRangeX = 120;
     private float spawnPosZ;
     private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    private int spawnInterval = 500;
+    public float spawnIntervalCurrent = 0;
+
     public GameObject player;
     public GameObject zombie;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnZombie", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(spawnIntervalCurrent);
+        if (spawnIntervalCurrent == spawnInterval)
+        {
+            SpawnZombie();
+            spawnIntervalCurrent = 0;
+        }
+        else 
+        { 
+            spawnIntervalCurrent += 5;
+        }
     }
 
     void SpawnZombie()
     {
-        spawnPosZ = player.transform.position.z + 400;
-
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
+        spawnPosZ = player.transform.position.z + 500;
+        Debug.Log(spawnPosZ);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX/2, spawnRangeX/2), 0, spawnPosZ);
         Instantiate(zombie, spawnPos, zombie.transform.rotation);
     }
 }
